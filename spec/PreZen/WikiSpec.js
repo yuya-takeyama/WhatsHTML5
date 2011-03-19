94713ea69d459015b1b063a9ed875c3e0c57474f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-03-19 22:03:24 +0900

    Enhancement: Line breaks are converted to <br />.

--- spec/PreZen/WikiSpec.js
+++ spec/PreZen/WikiSpec.js
@@ -2,6 +2,10 @@ var Wiki = require('PreZen/Wiki.js').Wiki;
 
 describe('Wiki', function () {
   describe('Wiki.convert()', function () {
+    it('should convert from "foo\nbar" to "<p>foo<br />bar</p>"', function () {
+      expect(Wiki.convert('foo\nbar')).toEqual('<p>foo<br />bar</p>');
+    });
+
     it('should convert from "foo\n\nbar" to "<p>foo</p><p>bar</p>".', function () {
       expect(Wiki.convert('foo\n\nbar')).toEqual('<p>foo</p><p>bar</p>');
     });
