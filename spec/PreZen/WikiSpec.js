f729c65bc482944feb09c4518dcdbc723b726da4
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-03-19 21:42:39 +0900

    1st commit.

---
+++ spec/PreZen/WikiSpec.js
@@ -0,0 +1,11 @@
+var Wiki = require('PreZen/Wiki.js').Wiki;
+
+console.dir(Wiki);
+
+describe('Wiki', function () {
+  describe('Wiki.convert("foo\nbar")', function () {
+    it('should be converted to String "<p>foo</p><p>bar</p>"', function () {
+      expect(Wiki.convert('foo\nbar')).toEqual('<p>foo</p><p>bar</p>');
+    });
+  });
+});
