880b85a505cb854e4991be3badde0a59453caa07
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-03-19 21:59:00 +0900

    Change: Blocks are splitted by two breaks.

--- spec/PreZen/WikiSpec.js
+++ spec/PreZen/WikiSpec.js
@@ -2,8 +2,8 @@ var Wiki = require('PreZen/Wiki.js').Wiki;
 
 describe('Wiki', function () {
   describe('Wiki.convert()', function () {
-    it('should convert from "foo\nbar" to "<p>foo</p><p>bar</p>".', function () {
-      expect(Wiki.convert('foo\nbar')).toEqual('<p>foo</p><p>bar</p>');
+    it('should convert from "foo\n\nbar" to "<p>foo</p><p>bar</p>".', function () {
+      expect(Wiki.convert('foo\n\nbar')).toEqual('<p>foo</p><p>bar</p>');
     });
   });
 });
