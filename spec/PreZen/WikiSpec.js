23a8488fc74abf2f15f4e1cda4938e38cfa2f4f3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-03-19 21:46:08 +0900

    Change: Spec text.

--- spec/PreZen/WikiSpec.js
+++ spec/PreZen/WikiSpec.js
@@ -1,8 +1,8 @@
 var Wiki = require('PreZen/Wiki.js').Wiki;
 
 describe('Wiki', function () {
-  describe('Wiki.convert("foo\nbar")', function () {
-    it('should be converted to String "<p>foo</p><p>bar</p>"', function () {
+  describe('Wiki.convert()', function () {
+    it('should convert from "foo\nbar" to "<p>foo</p><p>bar</p>".', function () {
       expect(Wiki.convert('foo\nbar')).toEqual('<p>foo</p><p>bar</p>');
     });
   });
