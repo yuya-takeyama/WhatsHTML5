ca83f07c2fefbded2fc5c567144ac3c74c114ed3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-03-19 21:43:12 +0900

    Remove: Debug code.

--- spec/PreZen/WikiSpec.js
+++ spec/PreZen/WikiSpec.js
@@ -1,7 +1,5 @@
 var Wiki = require('PreZen/Wiki.js').Wiki;
 
-console.dir(Wiki);
-
 describe('Wiki', function () {
   describe('Wiki.convert("foo\nbar")', function () {
     it('should be converted to String "<p>foo</p><p>bar</p>"', function () {
